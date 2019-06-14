bf89403bf6c57800392b7be7d4cd32cbe918300b
crmendez <crmendez@hotmail.com.ar>
2019-06-14 20:38:24 -0300

    subida de repositorio

---
+++ src/main/java/com/taller/taller/controller/ClienteController.java
@@ -0,0 +1,77 @@
+package com.taller.taller.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.taller.taller.Exception.ResourceNotFoundException;
+import com.taller.taller.model.Cliente;
+import com.taller.taller.repository.ClienteRepository;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping("/api")
+public class ClienteController {
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@GetMapping(value = "/clientes") 
+	public List<Cliente> getCliente() {
+        return clienteRepository.findAll(Sort.by("id"));
+        }
+	
+	@GetMapping(value = "/clientes/{id}") 
+	   public Cliente findByCustomerId (@PathVariable Integer id){ 
+	         return clienteRepository.findById(id).orElseThrow(()
+	        		 -> new ResourceNotFoundException("Cliente "+id+" no encontrado"));
+	         }
+	
+	@PostMapping(value = "/clientes")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Cliente save(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	 }
+	
+	@PutMapping(value = "/clientes/{id}")
+	public ResponseEntity<Cliente> updateCliente(@PathVariable Integer id,
+			@RequestBody Cliente nuevoCliente){
+		
+		return clienteRepository.findById(id).map(cliente -> {
+			cliente.setNombres(nuevoCliente.getNombres());
+			cliente.setApellido(nuevoCliente.getApellido());
+			cliente.setDireccion(nuevoCliente.getDireccion());
+			cliente.setCod_postal(nuevoCliente.getCod_postal());
+			cliente.setTelefono(nuevoCliente.getTelefono());
+			clienteRepository.save(cliente);
+			return ResponseEntity.ok(cliente);
+		}).orElseThrow(() -> new ResourceNotFoundException
+				("Cliente "+id+" no encontrado"));
+		
+	}
+	
+	@DeleteMapping(value = "/clientes/{id}")
+	public ResponseEntity<?> deleteCliente(@PathVariable Integer id){
+
+		return clienteRepository.findById(id).map(cliente -> {
+		clienteRepository.delete(cliente);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+        ).orElseThrow(() -> new ResourceNotFoundException
+        		("Cliente "+id+" no encontrado"));
+	
+    }
+	
+
+}
---
+++ src/main/java/com/taller/taller/model/Cliente.java
@@ -0,0 +1,75 @@
+package com.taller.taller.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="nombres")
+	private String nombres;
+	
+	@Column(name="apellido")
+	private String apellido;
+	
+	@Column(name="direccion")
+	private String direccion;
+	
+	@Column(name="cod_postal")
+	private String cod_postal;
+	
+	@Column(name="telefono")
+	private String telefono;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombres() {
+		return nombres;
+	}
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getCod_postal() {
+		return cod_postal;
+	}
+	public void setCod_postal(String cod_postal) {
+		this.cod_postal = cod_postal;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nombres=" + nombres + ", apellido=" + apellido + ", direccion=" + direccion
+				+ ", cod_postal=" + cod_postal + ", telefono=" + telefono + "]";
+	}
+	
+}
